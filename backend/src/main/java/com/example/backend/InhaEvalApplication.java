00d02f7bf623d9d332c7b6321b7f353ef21fda03
YeongJun Yoo <yyj406@naver.com>
2026-02-23 17:04:35 +0900

    feat: Maven -> Gradle 프로젝트 변경

--- backend/src/main/java/com/example/backend/InhaEvalApplication.java
+++ backend/src/main/java/com/example/backend/InhaEvalApplication.java
@@ -1,13 +1,13 @@
-package com.inhaeval.backend;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class InhaEvalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(InhaEvalApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/backend/InhaEvalApplicationTests.java
+++ backend/src/test/java/com/example/backend/InhaEvalApplicationTests.java
@@ -1,10 +1,10 @@
-package com.inhaeval.backend;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class InhaEvalApplicationTests {
 
 	@Test
 	void contextLoads() {
